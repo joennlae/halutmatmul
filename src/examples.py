aacdd2172d58976f3562cb4ffff41dcacb89ee83
Jannis Schönleber <joennlae@gmail.com>
2022-03-16 23:43:41 +0100

    feat(example): update python example

--- src/examples.py
+++ src/examples.py
@@ -1,19 +1,13 @@
 import numpy as np
 import maddness.maddness as mn
 
-if __name__ == "__main__":
-    print("test")
-    A_1 = np.random.random((50000, 512))
-    A_2 = np.random.random((50000, 512))
-    print(A_1.shape)
-    print(A_2.shape)
-    B = np.random.random((512, 50))
-    print(B)
-    C = np.matmul(A_2, B)
-    print(C)
+A_1 = np.random.random((50000, 512))
+A_2 = np.random.random((50000, 512))
+B = np.random.random((512, 50))
+C = np.matmul(A_2, B)
 
-    C_halut = mn.matmul(A_2, B, C=128, A_learn=A_1)
-    print(C_halut)
+C_halut = mn.matmul(A_2, B, C=128, A_learn=A_1)
+print(C_halut)
 
-    mse = np.square(C_halut - C).mean()
-    print(mse)
+mse = np.square(C_halut - C).mean()
+print(mse)
