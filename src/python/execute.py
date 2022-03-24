9952f7ae399ffe6dc0669272689a05f8d86664a6
Jannis Schönleber <joennlae@gmail.com>
2022-03-24 17:02:28 +0100

    feat(resnet): add pretrained CIFAR-10 resnet

---
+++ src/python/execute.py
@@ -0,0 +1,69 @@
+# pylint: disable=C0209
+import os
+
+import torchvision
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms as T
+
+from ResNet.resnet import resnet50
+
+script_dir = os.path.dirname(__file__)
+state_dict = torch.load(script_dir + "/.data/" + "resnet50" + ".pt", map_location="cpu")
+
+dict_ = state_dict
+print(dict_.keys())
+
+val_transform = T.Compose(
+    [
+        T.Resize(32),
+        T.CenterCrop(32),
+        T.ToTensor(),
+        T.Normalize((0.4914, 0.4822, 0.4465), (0.2471, 0.2435, 0.2616)),
+    ]
+)
+
+cifar_10_val = torchvision.datasets.CIFAR10(
+    root="./.data", train=False, transform=val_transform, download=False
+)
+
+loaded_data = DataLoader(
+    cifar_10_val, batch_size=128, num_workers=8, drop_last=False, pin_memory=True
+)
+print(loaded_data)
+
+model = resnet50(weights=state_dict)
+print(model)
+model.eval()
+
+correct_1 = 0.0
+correct_5 = 0.0
+total = 0
+
+with torch.no_grad():
+    for n_iter, (image, label) in enumerate(loaded_data):
+        # if n_iter > 10:
+        #     continue
+        print("iteration: {}\ttotal {} iterations".format(n_iter + 1, len(loaded_data)))
+        # https://github.com/weiaicunzai/pytorch-cifar100/blob/2149cb57f517c6e5fa7262f958652227225d125b/test.py#L54
+
+        output = model(image).squeeze(0).softmax(0)
+
+        _, pred = output.topk(5, 1, largest=True, sorted=True)
+
+        label = label.view(label.size(0), -1).expand_as(pred)
+
+        correct = pred.eq(label).float()
+
+        # compute top 5
+        correct_5 += correct[:, :5].sum()
+
+        # compute top1
+        correct_1 += correct[:, :1].sum()
+
+print(correct_1, correct_5)
+print("Top 1 error: ", 1 - correct_1 / len(loaded_data.dataset))
+print("Top 5 error: ", 1 - correct_5 / len(loaded_data.dataset))
+print("Top 1 accuracy: ", correct_1 / len(loaded_data.dataset))
+print("Top 5 accuracy: ", correct_5 / len(loaded_data.dataset))
+print("Parameter numbers: {}".format(sum(p.numel() for p in model.parameters())))
