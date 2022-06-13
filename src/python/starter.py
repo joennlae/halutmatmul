9b6bc69e04c493cfcc8295fa2e44ecb0ec24e22b
Jannis Schönleber <joennlae@gmail.com>
2022-06-13 14:53:33 +0200

    feat(results): update single-layer results

--- src/python/starter.py
+++ src/python/starter.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     # test_read_acc_lut_kernel(4096, 256, 128, 64, 1.0, 0.0)
     # test_conv2d_module_gpu(128, 128, 7, 1, False, 32, 1.0, 0.0)
     # test_conv2d_module_gpu(64, 64, 7, 1, False, 16, 16, 1.0, 0.0, 0, 2)
-    test_cifar10_inference()
-    # run_kws_main()
+    # test_cifar10_inference()
+    run_kws_main()
     # run_levit()
