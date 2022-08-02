344fe9b433c555d0bacd2f04f443bd6b26b0be2a
Jannis Schönleber <joennlae@gmail.com>
2022-08-02 18:14:25 +0200

    feat(openroad): adding `nangate45` support

--- hardware/flow/nangate45/config.mk
+++ hardware/flow/nangate45/config.mk
@@ -1,15 +1,12 @@
-export DESIGN_NAME = sram
+export DESIGN_NAME = register_file_latch
 export PLATFORM    = nangate45
 
-export VERILOG_FILES = ./designs/src/$(DESIGN_NAME)/sram.v
+export VERILOG_FILES = $(shell cat "files.txt")
 export SDC_FILE      = ./constraint.sdc
 export ABC_AREA      = 1
 
 # Adders degrade GCD
 export ADDER_MAP_FILE :=
 
-# These values must be multiples of placement site
-# x=0.19 y=1.4
-export DIE_AREA    = 0 0 2000 2000
-export CORE_AREA   = 10 10 1950 1950
-
+export DIE_AREA    = 0 0 200 200
+export CORE_AREA   = 10 10 195 195
