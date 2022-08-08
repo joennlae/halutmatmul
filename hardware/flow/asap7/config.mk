9a09d477ce9f0f83a1ead7c0d4e89c3a00c2e295
Jannis Schönleber <joennlae@gmail.com>
2022-08-09 01:12:05 +0200

    feat(OpenROAD): update `ASAP7` and `NanGate45` config files

--- hardware/flow/asap7/config.mk
+++ hardware/flow/asap7/config.mk
@@ -7,10 +7,10 @@ export SDC_FILE      = ./constraint.sdc
 
 export PLACE_DENSITY          = 0.60
 
-export DIE_AREA               = 0 0 30 30
-export CORE_AREA              = 2 2 28 28
+export DIE_AREA               = 0 0 100 100
+export CORE_AREA              = 2 2 98 98
 
-export HAS_IO_CONSTRAINTS = 1 # fix error [ERROR GPL-0305] RePlAce diverged at newStepLength.
+# export HAS_IO_CONSTRAINTS = 1 # fix error [ERROR GPL-0305] RePlAce diverged at newStepLength.
 
 export CORNER									= TC
 export DFF_LIB_FILE           = $($(CORNER)_DFF_LIB_FILE)
