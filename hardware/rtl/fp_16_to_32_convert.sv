4c54d6306851afb68361ff0c5457215bf69865aa
Jannis Schönleber <joennlae@gmail.com>
2022-08-14 20:33:54 +0200

    fix(yosys): fix synthesis by removing `assign` out of `always_comb`

--- hardware/rtl/fp_16_to_32_convert.sv
+++ hardware/rtl/fp_16_to_32_convert.sv
@@ -2,9 +2,9 @@ module fp_16_to_32_convert (
   input  logic [16-1:0] operand_fp16_i,
   output logic [32-1:0] result_o
 );
-  localparam int FP_16_EXP_BIAS = 15;
-  localparam int FP_32_EXP_BIAS = 127;
-  localparam int EXP_BIAS_CONVERSION = FP_32_EXP_BIAS - FP_16_EXP_BIAS;  // 112
+  localparam int unsigned FP_16_EXP_BIAS = 15;
+  localparam int unsigned FP_32_EXP_BIAS = 127;
+  localparam int unsigned EXP_BIAS_CONVERSION = FP_32_EXP_BIAS - FP_16_EXP_BIAS;  // 112
 
   logic sign;
   logic [4:0] exp_fp16_in;
@@ -30,16 +30,16 @@ module fp_16_to_32_convert (
 
   always_comb begin
     if (exp_fp16_in == 5'b00000) begin : denormal_subnormal_case
-      if (no_ones) begin : zero_case
-        assign exp_fp32_converted = 8'b0;
-        assign mant_converted = mant_fp16_in;
+      if (no_ones == 1'b1) begin : zero_case
+        exp_fp32_converted = 8'h0;
+        mant_converted = mant_fp16_in;
       end else begin : subnormal_case
-        assign exp_fp32_converted = 1 + 8'(EXP_BIAS_CONVERSION) - 8'(first_one) - 1;
-        assign mant_converted = mant_fp16_in << first_one + 1;
+        exp_fp32_converted = 8'(EXP_BIAS_CONVERSION) - 8'(first_one);  // + 1 - 1
+        mant_converted = mant_fp16_in << first_one + 1;
       end
     end else begin : default_case
-      assign exp_fp32_converted = 8'(exp_fp16_in) + 8'(EXP_BIAS_CONVERSION);
-      assign mant_converted = mant_fp16_in;
+      exp_fp32_converted = 8'(exp_fp16_in) + 8'(EXP_BIAS_CONVERSION);
+      mant_converted = mant_fp16_in;
     end
   end
   assign operand_fp32_converted = {sign, exp_fp32_converted, mant_converted, {13'h0}};
