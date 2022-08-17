29bc66446847588894945bcc2c0f859494966fa9
Jannis Schönleber <joennlae@gmail.com>
2022-08-17 12:55:08 +0200

    test(hw): add `halut_4_encoder` tests

---
+++ hardware/dv/halut_encoder_4/test_halut_encoder_4.py
@@ -0,0 +1,104 @@
+# pylint: disable=no-value-for-parameter, protected-access
+from math import log2
+from random import getrandbits
+import typing
+import numpy as np
+import cocotb
+from cocotb.triggers import RisingEdge
+from cocotb.clock import Clock
+from cocotb.binary import BinaryValue
+from cocotb.types import LogicArray
+
+
+from util.helper_functions import (
+    convert_fp16_array,
+    convert_int_array_width,
+    encoding_function,
+)
+
+DATA_TYPE_WIDTH = 16
+C = 32
+K = 16
+
+EncUnits = 4
+CPerEncUnit = C // EncUnits
+ThreshMemAddrWidth = int(log2(CPerEncUnit * K))
+TreeDepth = int(log2(K))
+
+ROWS = 64
+
+
+@cocotb.test()
+async def halut_encoder_4_test(dut) -> None:  # type: ignore[no-untyped-def]
+    # generate threshold table
+    threshold_table = np.random.random((C * K)).astype(np.float16)
+    input_a = np.random.random((ROWS, C, TreeDepth)).astype(np.float16)
+
+    encoded, kaddr_hist, thres_mem_hist = encoding_function(
+        threshold_table, input_a, tree_depth=TreeDepth, K=K
+    )
+
+    print("encoded", encoded, kaddr_hist, thres_mem_hist)
+    cocotb.start_soon(Clock(dut.clk_i, 2, units="ns").start())
+
+    # Initial values
+    dut.a_input_i.value = BinaryValue(0, n_bits=EncUnits * 4 * 16, bigEndian=True)
+    dut.waddr_i.value = BinaryValue(0, n_bits=EncUnits * 16, bigEndian=True)
+    dut.wdata_i.value = BinaryValue(0, n_bits=EncUnits * 16, bigEndian=True)
+    dut.we_i.value = BinaryValue(0, n_bits=EncUnits, bigEndian=True)
+    dut.encoder_i.value = 0
+
+    # Reset DUT
+    dut.rst_ni.value = 0
+    for _ in range(6):
+        await RisingEdge(dut.clk_i)
+    dut.rst_ni.value = 1
+
+    # write threshold values
+
+    await RisingEdge(dut.clk_i)
+    for idx in range(threshold_table.shape[0] // EncUnits):
+        w_addr_val = convert_int_array_width(
+            [idx, idx, idx, idx], n_bits=ThreshMemAddrWidth
+        )
+        dut.we_i.value = LogicArray([1, 1, 1, 1])
+        ids = np.arange(EncUnits) * 16 + (idx // 16 * EncUnits * 16) + (idx % 16)
+        dut.waddr_i.value = w_addr_val
+        dut.wdata_i.value = convert_fp16_array(threshold_table[ids])
+        # dut._log.info(
+        #     f"write {dut.waddr_i.value}, {w_addr_val}, {ids}, {dut.wdata_i.value}"
+        # )
+        await RisingEdge(dut.clk_i)
+    dut.we_i.value = LogicArray([0, 0, 0, 0])
+
+    current_encoder_input = np.zeros((EncUnits * 4), dtype=np.float16)
+    idx_encoder_input_base = np.arange(4) * 4
+    idx_encoder_input_top = np.arange(4) * 4 + 4
+    dut.encoder_i.value = 1
+    await RisingEdge(dut.clk_i)
+    for row in range(input_a.shape[0]):
+        for c_ in range(input_a.shape[1]):
+            current_encoder_input[
+                idx_encoder_input_base[c_ % 4] : idx_encoder_input_top[c_ % 4]
+            ] = input_a[row, c_]
+            dut.a_input_i.value = convert_fp16_array(current_encoder_input)
+            await RisingEdge(dut.clk_i)
+            # dut._log.info(
+            #     f"(int_vals) : {dut.encoder_int.value}, {dut.k_addr_o_q.value}, "
+            #     f"{dut.c_addr_o_q.value}, {dut.valid_counter.value}, {dut.k_addr_int.value}"
+            #     f"{dut.valid_int.value}, {dut.c_addr_int.value}, {c_}"
+            # )
+            # do asserts
+            if not (row == 0 and c_ < 5):
+                lookup_row = row - 1 if c_ < 5 else row
+                lookup_c = np.arange(C)[c_ - 5]
+                # dut._log.info(
+                #     f"(assert) : {dut.valid_o.value}, {dut.k_addr_o.value}, "
+                #     f"{dut.c_addr_o.value}\n"
+                #     f"(compare): 1, {encoded[lookup_row, c_ - 5]}, {lookup_c}"
+                # )
+                assert dut.valid_o.value == 1, "output not valid"
+                assert (
+                    dut.k_addr_o.value.value == encoded[lookup_row, c_ - 5]
+                ), "encoded output wrong"
+                assert dut.c_addr_o.value.value == lookup_c, "assert wrong c output"
--- hardware/dv/util/helper_functions.py
+++ hardware/dv/util/helper_functions.py
@@ -11,6 +11,15 @@ def convert_fp16_array(vals: np.ndarray) -> BinaryValue:
     return fuse_binary_values(bin_vals)
 
 
+def convert_int_array_width(vals: "typing.List[int]", n_bits: int = 4) -> BinaryValue:
+    bin_vals = []
+    for val in vals:
+        bin_vals.append(
+            BinaryValue(val, n_bits=n_bits, bigEndian=False)
+        )  # hack to get bigEndian!!
+    return fuse_binary_values(bin_vals)
+
+
 def fuse_binary_values(vals: "typing.List[BinaryValue]") -> BinaryValue:
     tot_binstr = ""
     for val in vals:
@@ -54,3 +63,35 @@ def binary_to_float32(binary: BinaryValue) -> np.float32:
     dt = np.dtype(np.float32)
     dt = dt.newbyteorder(">")
     return np.frombuffer(bytes(bytes_list), dtype=dt, count=-1)[0]
+
+
+def encoding_function(
+    threshold_table: np.ndarray, input_a: np.ndarray, tree_depth: int = 4, K: int = 16
+) -> "typing.Tuple[np.ndarray, np.ndarray, np.ndarray]":
+    encoded = np.zeros((input_a.shape[0], input_a.shape[1]), dtype=np.int32)
+    kaddr_history = np.zeros(
+        (input_a.shape[0], input_a.shape[1], tree_depth), dtype=np.int32
+    )
+    thresh_mem_history = np.zeros(
+        (input_a.shape[0], input_a.shape[1], tree_depth), dtype=np.float16
+    )
+    # CPerEncUnit = input_a.shape[1]
+    caddr_internal_offset = np.arange(input_a.shape[1]) * K
+    prototype_addr_internal_offset = 2 ** np.arange(tree_depth + 1) - 1
+    for row in range(input_a.shape[0]):
+        kaddr = np.zeros(input_a.shape[1], dtype=np.int64)
+        prototype_addr_internal = np.zeros(input_a.shape[1], dtype=np.int64)
+        for tree_level_cnt in range(tree_depth):
+            kaddr_history[row, :, tree_level_cnt] = kaddr
+            data_thresh_mem_o = threshold_table[
+                prototype_addr_internal + caddr_internal_offset
+            ]
+            thresh_mem_history[row, :, tree_level_cnt] = data_thresh_mem_o
+            data_input_comparision = input_a[row, :, tree_level_cnt]
+            fp_16_comparision_o = data_input_comparision > data_thresh_mem_o
+            kaddr = (kaddr * 2) + fp_16_comparision_o
+            prototype_addr_internal = (
+                kaddr + prototype_addr_internal_offset[tree_level_cnt + 1]
+            )
+        encoded[row] = kaddr
+    return encoded, kaddr_history, thresh_mem_history
