e1a692ecac0a74bf5fa2a2ce14b7d440def68ba0
Jannis Schönleber <joennlae@gmail.com>
2022-09-17 19:57:13 +0200

    feat(sweep): add `patchelf` script to install fast on new machine

---
+++ hardware/scripts/patchelf_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CONDA_ENV_PATH=/scratch/janniss/conda/halutmatmul_hw
+patchelf --set-rpath ~/.local/custom_glibc/lib:~/.local/lib64:/lib64 --set-interpreter ~/.local/custom_glibc/lib/ld-linux-x86-64.so.2 /scratch/janniss/conda/halutmatmul_hw/bin/zachjs-sv2v
+
+patchelf --set-rpath ~/.local/custom_glibc/lib:~/.local/lib64 --set-interpreter ~/.local/custom_glibc/lib/ld-linux-x86-64.so.2 /scratch/janniss/conda/halutmatmul_hw/bin/verible-verilog-format
+patchelf --set-rpath ~/.local/custom_glibc/lib:~/.local/lib64 --set-interpreter ~/.local/custom_glibc/lib/ld-linux-x86-64.so.2 /scratch/janniss/conda/halutmatmul_hw/bin/verible-verilog-syntax
