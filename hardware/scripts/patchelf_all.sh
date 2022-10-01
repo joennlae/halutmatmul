3ebd430e1d91e7a8e5e954a77f6104b990e2682e
Jannis Schönleber <joennlae@gmail.com>
2022-10-01 22:15:56 +0200

    feat(env): add `verible-verilog-lint` to `patchelf` script

--- hardware/scripts/patchelf_all.sh
+++ hardware/scripts/patchelf_all.sh
@@ -5,3 +5,4 @@ patchelf --set-rpath ~/.local/custom_glibc/lib:~/.local/lib64:/lib64 --set-inter
 
 patchelf --set-rpath ~/.local/custom_glibc/lib:~/.local/lib64 --set-interpreter ~/.local/custom_glibc/lib/ld-linux-x86-64.so.2 /scratch/janniss/conda/halutmatmul_hw/bin/verible-verilog-format
 patchelf --set-rpath ~/.local/custom_glibc/lib:~/.local/lib64 --set-interpreter ~/.local/custom_glibc/lib/ld-linux-x86-64.so.2 /scratch/janniss/conda/halutmatmul_hw/bin/verible-verilog-syntax
+patchelf --set-rpath ~/.local/custom_glibc/lib:~/.local/lib64 --set-interpreter ~/.local/custom_glibc/lib/ld-linux-x86-64.so.2 /scratch/janniss/conda/halutmatmul_hw/bin/verible-verilog-lint
