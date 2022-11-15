e6781b39c57b2e5a55f7b649dc5e88ee4080fe95
Jannis Schönleber <joennlae@gmail.com>
2022-11-15 12:38:59 +0100

    feat(backprop): add `linear` and `conv2d` `autograd.Function`

--- src/python/backprop/custom_autograd_functions.py
+++ src/python/backprop/custom_autograd_functions.py
@@ -1,4 +1,10 @@
+# pylint: disable=abstract-method, arguments-differ
+from typing import Optional
 import torch
+from torch.functional import Tensor
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+from halutmatmul.modules import HalutLinear
 
 # torch.autograd.Function
 # source https://pytorch.org/docs/master/notes/extending.html#example
@@ -8,12 +14,34 @@ class LinearFunction(torch.autograd.Function):
     # Note that both forward and backward are @staticmethods
     @staticmethod
     # bias is an optional argument
-    def forward(ctx, input, weight, bias=None):
+    def forward(
+        ctx,
+        input: Tensor,
+        weight: Tensor,
+        bias: Optional[Tensor] = None,
+        self_module: Optional[HalutLinear] = None,
+    ) -> Tensor:
         ctx.save_for_backward(input, weight, bias)
-        output = input.mm(weight.t())
-        if bias is not None:
-            output += bias.unsqueeze(0).expand_as(output)
-        return output
+        if self_module is None or not self_module.halut_active[0]:
+            output = input.mm(weight.t())
+
+            if bias is not None:
+                output += bias.unsqueeze(0).expand_as(output)
+            return output
+        # halut
+        if self_module is None:
+            raise Exception("self_model is None when halut active!!")
+
+        # new lut calculated
+        # TODO: check if weight needs to be transposed
+        torch_res = torch.tensordot(
+            self_module.prototypes, weight.t(), dims=([2], [0])
+        ).permute((2, 0, 1))
+        self_module.lut = Parameter(
+            torch_res,
+            requires_grad=False,
+        )
+        return self_module.linear_halut(input)
 
     # This function has only a single output, so it gets only one gradient
     @staticmethod
@@ -39,4 +67,36 @@ class LinearFunction(torch.autograd.Function):
 
         return grad_input, grad_weight, grad_bias
 
+
 halutlinear = LinearFunction.apply
+
+
+class Conv2dFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(
+        cxt, input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1
+    ):
+
+        cxt.save_for_backward(input, weight, bias)
+
+        return F.conv2d(input, weight, bias, stride, padding, dilation, groups)
+
+    @staticmethod
+    def backward(cxt, grad_output):
+        input, weight, bias = cxt.saved_variables
+
+        grad_input = grad_weight = grad_bias = None
+
+        if cxt.needs_input_grad[0]:
+            grad_input = torch.nn.grad.conv2d_input(input.shape, weight, grad_output)
+
+        if cxt.needs_input_grad[1]:
+            grad_weight = torch.nn.grad.conv2d_weight(input, weight.shape, grad_output)
+
+        if bias is not None and cxt.needs_input_grad[2]:
+            grad_bias = grad_output.sum((0, 2, 3)).squeeze(0)
+
+        return grad_input, grad_weight, grad_bias
+
+
+halutconv2d = Conv2dFunction.apply
