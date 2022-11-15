6bf33f1b05763f7dfddff37a8207a0266bbba5d1
Jannis Schönleber <joennlae@gmail.com>
2022-11-15 16:45:39 +0100

    feat(backprop): lint + add `halut` to `conv2d` custom path

--- src/python/backprop/gradient_check.py
+++ src/python/backprop/gradient_check.py
@@ -13,8 +13,13 @@ input = (
 test = gradcheck(halutlinear, input, eps=1e-6, atol=1e-4)
 print(test)
 input_conv = (
-    torch.randn(20, 20, 32, 32, dtype=torch.double, requires_grad=True),
-    torch.randn(30, 20, 32, 32, dtype=torch.double, requires_grad=True),
+    torch.randn(6, 6, 32, 32, dtype=torch.double, requires_grad=True),
+    torch.randn(6, 6, 32, 32, dtype=torch.double, requires_grad=True),
+    None,
+    1,
+    0,
+    1,
+    1,
 )
 test = gradcheck(halutconv2d, input_conv, eps=1e-6, atol=1e-4)
 print(test)
