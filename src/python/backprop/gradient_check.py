f1c710be789bae6a77fb1eac7d549e3507173cf4
Jannis Schönleber <joennlae@gmail.com>
2022-11-15 12:38:59 +0100

    feat(backprop): add `linear` and `conv2d` `autograd.Function`

--- src/python/backprop/gradient_check.py
+++ src/python/backprop/gradient_check.py
@@ -1,7 +1,8 @@
 import torch
 
 from torch.autograd import gradcheck
-from backprop.custom_autograd_functions import halutlinear
+from backprop.custom_autograd_functions import halutlinear, halutconv2d
+
 # gradcheck takes a tuple of tensors as input, check if your gradient
 # evaluated with these tensors are close enough to numerical
 # approximations and returns True if they all verify this condition.
@@ -10,4 +11,10 @@ input = (
     torch.randn(30, 20, dtype=torch.double, requires_grad=True),
 )
 test = gradcheck(halutlinear, input, eps=1e-6, atol=1e-4)
+print(test)
+input_conv = (
+    torch.randn(20, 20, 32, 32, dtype=torch.double, requires_grad=True),
+    torch.randn(30, 20, 32, 32, dtype=torch.double, requires_grad=True),
+)
+test = gradcheck(halutconv2d, input_conv, eps=1e-6, atol=1e-4)
 print(test)
