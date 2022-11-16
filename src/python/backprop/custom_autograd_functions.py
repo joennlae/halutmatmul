fix(backprop): fix `linear` custom backprop

src/python/backprop/custom_autograd_functions.py
@@ -68,7 +68,7 @@ class LinearFunction(torch.autograd.Function):
         if bias is not None and ctx.needs_input_grad[2]:
             grad_bias = grad_output.sum(0)
 
-        return grad_input, grad_weight, grad_bias
+        return grad_input, grad_weight, grad_bias, None
 
 
 halutlinear = LinearFunction.apply