b6fb6b96474735bc153764aa8d8a94b3ea2dee46
Jannis Schönleber <joennlae@gmail.com>
2022-11-24 11:19:08 +0100

    test(backprop): update `HalutLinear` test for new path

--- src/python/starter.py
+++ src/python/starter.py
@@ -1,7 +1,5 @@
-from test.test_conv2d import test_conv2d_module
-from test.test_resnet import test_cifar10_inference
-from test.test_conv2d_gpu import test_conv2d_module_gpu
 from test.test_halut import test_learn_offline
+from test.test_linear import test_linear_module
 from models.dscnn.main import run_kws_main
 from models.levit.main import run_levit  # type: ignore[attr-defined]
 from halutmatmul.halutmatmul import EncodingAlgorithm
@@ -17,5 +15,7 @@ if __name__ == "__main__":
     # test_conv2d_module_gpu(128, 128, 7, 1, False, 32, 1.0, 0.0)
     # test_conv2d_module_gpu(64, 64, 7, 1, False, 16, 16, 1.0, 0.0, 0, 2)
     # test_cifar10_inference()
-    run_kws_main()
+    # run_kws_main()
     # run_levit()
+    # test_cifar10_inference()
+    test_linear_module(512, 128, 32, 1.0, 0.0, False, 8)
