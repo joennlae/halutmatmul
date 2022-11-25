c55b5f59e1fe3467aabcf3005d40138bce71f048
Jannis Schönleber <joennlae@gmail.com>
2022-11-25 12:12:37 +0100

    test(resnet18): fix `e2e` `ResNet-18` test w/ self trained weights

--- src/python/test/test_resnet.py
+++ src/python/test/test_resnet.py
@@ -4,14 +4,16 @@ import requests
 import torchvision
 import torch
 from torchvision import transforms as T
+from torchvision.transforms.functional import InterpolationMode
 
-from models.resnet import resnet50
+from models.resnet import resnet18
+from training import presets
 
 from halutmatmul.model import HalutHelper
 
 
 def download_weights(path: str, is_ci: bool = False) -> None:
-    url = "https://github.com/joennlae/PyTorch_CIFAR10/raw/ver2022/resnet50.pt"
+    url = "https://github.com/joennlae/PyTorch_CIFAR10/raw/ver2022/resnet18.pth"
 
     # Streaming, so we can iterate over the response.
     # pylint: disable=missing-timeout
@@ -22,8 +24,8 @@ def download_weights(path: str, is_ci: bool = False) -> None:
     block_size = 2**20  # Mebibyte
 
     dl = 0
-    print("Download ResNet-50 CIFAR-10 weights")
-    with open(path + "/" + "resnet50.pt", "wb") as f:
+    print("Download ResNet-18 CIFAR-10 weights")
+    with open(path + "/" + "resnet18.pth", "wb") as f:
         for data in r.iter_content(block_size):
             f.write(data)
             dl += len(data)
@@ -41,33 +43,28 @@ def test_cifar10_inference() -> None:
 
         script_dir = tmpdirname
         state_dict = torch.load(
-            script_dir + "/" + "resnet50" + ".pt", map_location="cpu"
+            script_dir + "/" + "resnet18" + ".pth", map_location="cpu"
         )
 
         # CIFAR transformation
-        val_transform = T.Compose(
-            [
-                T.Resize(32),
-                T.CenterCrop(32),
-                T.ToTensor(),
-                T.Normalize((0.4914, 0.4822, 0.4465), (0.2471, 0.2435, 0.2616)),
-            ]
+        preprocessing = presets.ClassificationPresetEval(
+            crop_size=32,
+            resize_size=32,
+            interpolation=InterpolationMode("bilinear"),
         )
 
         cifar_10_val = torchvision.datasets.CIFAR10(
             root=tmpdirname + "/.data",
             train=False,
-            transform=val_transform,
+            transform=preprocessing,
             download=True,
         )
 
-        model = resnet50(
-            weights=state_dict, progress=False, **{"is_cifar": True, "num_classes": 10}
-        )
+        model = resnet18(progress=True, **{"is_cifar": True, "num_classes": 10})
 
         halut_model = HalutHelper(
             model,
-            state_dict,
+            state_dict["model"],
             cifar_10_val,
             data_path=tmpdirname + "/.data",
             learned_path=tmpdirname + "/.data/learned",
@@ -75,8 +72,8 @@ def test_cifar10_inference() -> None:
             num_workers=2,
         )
         halut_model.print_available_module()
-        halut_model.activate_halut_module("fc", 16, 10000)
-        halut_model.activate_halut_module("layer4.2.conv3", 16, 10000)
+        halut_model.activate_halut_module("fc", 16, -1)
+        halut_model.activate_halut_module("layer1.1.conv2", 16, -1)
         accuracy = halut_model.run_inference()
         accuracy = halut_model.run_inference()  # check if stored used
-        assert accuracy >= 0.9318 * 100
+        assert accuracy >= 87.3
