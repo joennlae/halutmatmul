5597fd18ec1fd3d755e9789b2e385cf3baff5cf1
Jannis Schönleber <joennlae@gmail.com>
2022-11-28 15:15:12 +0100

    feat(backprop): updating `starter`

--- src/python/starter.py
+++ src/python/starter.py
@@ -1,4 +1,6 @@
-from test.test_resnet import test_cifar10_inference
+from test.test_linear import test_linear_module
+from test.test_conv2d import test_conv2d_module
 
 if __name__ == "__main__":
-    test_cifar10_inference()
+    # test_linear_module(2048, 512, 32, 1.0, 0.0, False, 32)
+    test_conv2d_module(512, 512, 6, 3, False, 64, 16, 1.0, 0.0, 1)
