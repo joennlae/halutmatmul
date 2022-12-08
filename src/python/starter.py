993f628dafb59a93004f73aaa6d4ccf7d00360cd
Jannis Schönleber <joennlae@gmail.com>
2022-12-08 15:34:15 +0100

    feat(A): `HalutConv2d` + `tests` work

--- src/python/starter.py
+++ src/python/starter.py
@@ -2,5 +2,5 @@ from test.test_linear import test_linear_module
 from test.test_conv2d import test_conv2d_module
 
 if __name__ == "__main__":
-    test_linear_module(512, 32, 32, 1.0, 0.0, False, 32, True)
-    # test_conv2d_module(512, 512, 6, 3, False, 64, 16, 1.0, 0.0, 1)
+    # test_linear_module(512, 32, 32, 1.0, 0.0, False, 32, True)
+    test_conv2d_module(512, 512, 6, 3, False, 64, 16, 1.0, 0.0, 1, True)
