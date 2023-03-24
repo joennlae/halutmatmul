e877bb0458ce7116ada5cb0d4632092bce672780
Jannis Schönleber <joennlae@gmail.com>
2023-03-24 11:15:15 +0100

    feat(lut): update `lut` test calculations

--- src/python/backprop/test_lut_calculation.py
+++ src/python/backprop/test_lut_calculation.py
@@ -31,3 +31,37 @@ torch_res = torch.tensordot(prop, weights, dims=([2], [0])).permute((2, 0, 1))  
 print(torch_res.shape)
 torch_numpy_res = torch_res.detach().numpy()
 print(np.allclose(torch_numpy_res, lut))
+
+
+d = 9
+D = C * d
+prototypes = np.random.random((C, K, d))
+W = np.random.random((D, M))
+W_reshaped = W.reshape((M, C, d)).transpose((2, 1, 0))
+
+lut = np.zeros((W.shape[1], C, K))
+# manual
+
+lut_very_simple = np.zeros((W.shape[1], C, K))
+
+for m in range(M):
+    for c in range(C):
+        for k in range(K):
+            for i in range(d):
+                lut_very_simple[m, c, k] += prototypes[c, k, i] * W_reshaped[i, c, m]
+
+for m in range(M):
+    for c in range(C):
+        for k in range(K):
+            # print("shapes", prototypes[c, k, :].shape, W_reshaped[:, c, m].shape)
+            lut[m, c, k] = np.dot(prototypes[c, k, :], W_reshaped[:, c, m])
+
+print("simple == very simple", np.allclose(lut, lut_very_simple))
+# single operation
+prop = torch.from_numpy(prototypes)
+weights = torch.from_numpy(W_reshaped)
+print("prop", prop.shape, "weights", weights.shape)
+torch_res = torch.einsum("CKd, dCM -> MCK", [prop, weights])  # type: ignore
+print(torch_res.shape)
+torch_numpy_res = torch_res.detach().numpy()
+print(np.allclose(torch_numpy_res, lut))
