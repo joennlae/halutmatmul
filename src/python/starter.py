9710325d68a4ab35f4c5a3ed19c43035c224e2e9
Jannis Schönleber <joennlae@gmail.com>
2023-03-29 09:35:04 +0200

    chore: update `starter.py`

--- src/python/starter.py
+++ src/python/starter.py
@@ -1,10 +1,16 @@
 from test.test_linear import test_linear_module
 from test.test_conv2d import test_conv2d_module
-from test.test_resnet import test_cifar10_inference
+from test.test_resnet import test_cifar10_inference_resnet20
+from utils.analysis_helper import resnet20_layers
 
 if __name__ == "__main__":
-    # test_linear_module(512, 32, 32, 1.0, 0.0, False, 32, False, True)
-    test_conv2d_module(
-        32, 32, 7, 3, False, 32, 16, 1.0, 0.0, 1, False, 2, 0, "im2col", False
-    )
-    # test_cifar10_inference()
+    # test_linear_module(128, 64, 16, 9.0, -0.35, False, 32, False, True)
+    # test_conv2d_module(
+    #     32, 32, 7, 3, False, 32, 16, 1.0, 0.0, 1, False, 2, 0, "im2col", False
+    # )
+    acc = {}
+    for layer in resnet20_layers[:3]:
+        accuracy = test_cifar10_inference_resnet20(layer)
+        acc[layer] = accuracy
+    print(acc)
+    # test_cifar10_inference_resnet20("layer1.2.conv1")
