c68fb5396f44e3fbb72a0a206aa2b3bd127a4abe
Jannis Schönleber <joennlae@gmail.com>
2023-03-31 15:26:59 +0200

    feat(resnet20): change to option B

--- src/python/models/resnet20.py
+++ src/python/models/resnet20.py
@@ -71,7 +71,7 @@ class LambdaLayer(nn.Module):
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1, option="A"):
+    def __init__(self, in_planes, planes, stride=1, option="B"):
         super(BasicBlock, self).__init__()
         self.conv1 = HalutConv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
--- src/python/utils/analysis_helper.py
+++ src/python/utils/analysis_helper.py
@@ -358,6 +358,29 @@ resnet20_layers = [
     "layer3.2.conv2",
     "linear",
 ]
+resnet20_b_layers = [
+    "layer1.0.conv1",
+    "layer1.0.conv2",
+    "layer1.1.conv1",
+    "layer1.1.conv2",
+    "layer1.2.conv1",
+    "layer1.2.conv2",
+    "layer2.0.conv1",
+    "layer2.0.conv2",
+    "layer2.0.shortcut.0",
+    "layer2.1.conv1",
+    "layer2.1.conv2",
+    "layer2.2.conv1",
+    "layer2.2.conv2",
+    "layer3.0.conv1",
+    "layer3.0.conv2",
+    "layer3.0.shortcut.0",
+    "layer3.1.conv1",
+    "layer3.1.conv2",
+    "layer3.2.conv1",
+    "layer3.2.conv2",
+    "linear",
+]
 
 """
 layer1.0.conv1   torch.Size([64, 64, 3, 3])
@@ -393,7 +416,7 @@ def get_layers(name: available_models) -> list[str]:
     elif name == "resnet18":
         return resnet18_layers
     elif name == "resnet20":
-        return resnet20_layers
+        return resnet20_b_layers
     else:
         return Exception("Model name not supported: ", name)  # type: ignore
 
