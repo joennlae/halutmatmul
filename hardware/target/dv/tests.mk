a1130563652f9fb5fac583f56e76b5e4f2a4548b
Jannis Schönleber <joennlae@gmail.com>
2023-08-14 11:28:00 +0200

    chore: update test targets name

--- hardware/target/dv/tests.mk
+++ hardware/target/dv/tests.mk
@@ -12,11 +12,11 @@ TESTS = \
 	fp-16-comparision \
 	fp-16-to-32-convert
 
-targets = $(addprefix test-, $(TESTS))
+test-targets = $(addprefix test-, $(TESTS))
 
-.PHONY: $(targets)
+.PHONY: $(test-targets)
 
-$(targets): test-%: $(HALUT_ROOT)/target/open-frontend/pickle/out/halut_matmul.sv2v.v
+$(test-targets): test-%: $(HALUT_ROOT)/target/open-frontend/pickle/out/halut_matmul.sv2v.v
 	make -C $(HALUT_ROOT)/target/dv/$*/ SIM=icarus HALUT_ROOT=$(HALUT_ROOT) PYTHONPATH=$(HALUT_ROOT)/target/dv/
 
-test-all: $(targets)
+test-all: $(test-targets)
