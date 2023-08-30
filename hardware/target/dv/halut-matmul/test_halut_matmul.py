e27ad883fbbf39d68c1e3465f606b2982328ae2c
Jannis Schönleber <joennlae@gmail.com>
2023-08-30 22:21:10 +0200

    test(int): update e2e tests support int accumulation

--- hardware/target/dv/halut-matmul/test_halut_matmul.py
+++ hardware/target/dv/halut-matmul/test_halut_matmul.py
@@ -18,16 +18,20 @@ from util.helper_functions import (
     create_bin_vals_from_binstr,
     decoding_2d,
     encoding_function,
+    binary_to_int32,
+    convert_int8_array,
 )
 
 CLOCK_PERIOD_PS = int(os.environ.get("CLK_PERIOD", 1000))
 print("CLOCK_PERIOD_PS = ", CLOCK_PERIOD_PS)
 
-DATA_TYPE_WIDTH = 16
+DATA_TYPE_WIDTH = int(os.environ.get("DATA_WIDTH", 16))
+ENC_DATA_TYPE_WIDTH = 16  # only support FP16 input for now
 C = int(os.environ.get("NUM_C", 32))
 K = 16
 M = int(os.environ.get("NUM_M", 32))
 DecoderUnits = int(os.environ.get("NUM_DECODER_UNITS", 16))
+ACC_TYPE = os.environ.get("ACC_TYPE", "FP32")
 
 EncUnits = 4
 DecUnitsX = M // DecoderUnits
@@ -46,14 +50,15 @@ ROWS = 16  # * 16
 async def halut_matmul_test(dut) -> None:  # type: ignore[no-untyped-def]
     # generate threshold table
     # np.random.seed(4419)
-    threshold_table = np.random.random((C * K)).astype(np.float16)
-    input_a = np.random.random((ROWS, C, TreeDepth)).astype(np.float16)
+    dtype = np.float16 if ACC_TYPE == "FP32" else np.int8
+    threshold_table = (np.random.random((C * K)) * 256 - 127).astype(np.float16)
+    input_a = (np.random.random((ROWS, C, TreeDepth)) * 256 - 127).astype(np.float16)
 
     encoded, _, _ = encoding_function(
         threshold_table, input_a, tree_depth=TreeDepth, K=K
     )
 
-    lut = np.random.random((M, C, K)).astype(np.float16)
+    lut = (np.random.random((M, C, K)) * 256 - 127).astype(dtype)
 
     result, _ = decoding_2d(lut, encoded)
     print("results", encoded, result)
@@ -61,13 +66,13 @@ async def halut_matmul_test(dut) -> None:  # type: ignore[no-untyped-def]
 
     # Initial values
     dut.a_input_enc_i.value = BinaryValue(
-        0, n_bits=EncUnits * TreeDepth * DATA_TYPE_WIDTH, bigEndian=True
+        0, n_bits=EncUnits * TreeDepth * ENC_DATA_TYPE_WIDTH, bigEndian=True
     )
     dut.waddr_enc_i.value = BinaryValue(
         0, n_bits=EncUnits * ThreshMemAddrWidth, bigEndian=True
     )
     dut.wdata_enc_i.value = BinaryValue(
-        0, n_bits=EncUnits * DATA_TYPE_WIDTH, bigEndian=True
+        0, n_bits=EncUnits * ENC_DATA_TYPE_WIDTH, bigEndian=True
     )
     dut.we_enc_i.value = BinaryValue(0, n_bits=EncUnits, bigEndian=True)
     dut.encoder_i.value = 0
@@ -116,7 +121,10 @@ async def halut_matmul_test(dut) -> None:  # type: ignore[no-untyped-def]
                 )
                 dut.we_dec_i.value = LogicArray([1 for _ in range(DecUnitsX)])
                 m_idx = np.arange(DecUnitsX) * DecoderUnits + m
-                dut.wdata_dec_i.value = convert_fp16_array(lut[m_idx, c, k])
+                if ACC_TYPE == "FP32":
+                    dut.wdata_dec_i.value = convert_fp16_array(lut[m_idx, c, k])
+                elif ACC_TYPE == "INT":
+                    dut.wdata_dec_i.value = convert_int8_array(lut[m_idx, c, k])
                 await RisingEdge(dut.clk_i)
         dut._log.info(f"finished writing {m + 1}/{DecoderUnits}")
 
@@ -134,6 +142,7 @@ async def halut_matmul_test(dut) -> None:  # type: ignore[no-untyped-def]
     dut.encoder_i.value = 1
     dut.a_input_enc_i.value = convert_fp16_array(current_encoder_input)
     await RisingEdge(dut.clk_i)
+    # pylint: disable=too-many-nested-blocks
     for row in range(input_a.shape[0] + 1):
         for c_ in range(input_a.shape[1]):
             if row < ROWS:
@@ -150,13 +159,13 @@ async def halut_matmul_test(dut) -> None:  # type: ignore[no-untyped-def]
                 dut.a_input_enc_i.value = convert_fp16_array(current_encoder_input)
 
             await RisingEdge(dut.clk_i)
-            # dut._log.info(
-            #     f"(r) : {row}, {c_}, \n"
-            #     f"{dut.valid_o.value}, {dut.result_o.value}, {dut.m_addr_o.value}\n"
-            #     f"{dut.valid_enc_o.value}"
-            # )
+            dut._log.info(
+                f"(r) : {row}, {c_}, \n"
+                f"{dut.valid_o.value}, {dut.result_o.value}, {dut.m_addr_o.value}\n"
+                f"{dut.valid_enc_o.value}"
+            )
             # do asserts encoder
-            if (not (row == 0 and c_ < 5)) and (not row == ROWS and c > 5):
+            if (not (row == 0 and c_ < 5)) and (not row == ROWS and c_ > 5):
                 lookup_row = row - 1 if c_ < 5 else row
                 lookup_c = np.arange(C)[c_ - 5]
                 assert dut.valid_enc_o.value == 1, "enc output not valid"
@@ -186,15 +195,21 @@ async def halut_matmul_test(dut) -> None:  # type: ignore[no-untyped-def]
                         assert (
                             m_addr_bin_vals[i] == lookup_m + DecoderUnits * i
                         ), f"m_addr_o[{i}] wrong"
-                        assert (
-                            binary_to_float32(result_o_bin_vals[i])
-                            == result[row - 1, lookup_m + DecoderUnits * i]
-                        ), "result_o[{i}] wrong"
+                        if ACC_TYPE == "FP32":
+                            assert (
+                                binary_to_float32(result_o_bin_vals[i])
+                                == result[row - 1, lookup_m + DecoderUnits * i]
+                            ), "result_o[{i}] wrong"
+                        elif ACC_TYPE == "INT":
+                            assert (
+                                binary_to_int32(result_o_bin_vals[i])
+                                == result[row - 1, lookup_m + DecoderUnits * i]
+                            ), "result_o[{i}] wrong"
                 else:
                     assert dut.valid_o.value == BinaryValue(
                         0, n_bits=DecUnitsX
                     ), "should be invalid"
-                if row == ROWS and c == 7 + DecoderUnits - 7:
+                if row == ROWS and c_ == 7 + DecoderUnits - 7:
                     dut.encoder_i.value = 0  # turn off
             else:
                 assert dut.valid_o.value == BinaryValue(
