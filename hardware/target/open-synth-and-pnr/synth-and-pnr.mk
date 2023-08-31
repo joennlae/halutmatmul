90efa365af4b324945581fb9283271d357074cf9
Jannis Schönleber <joennlae@gmail.com>
2023-08-31 14:28:25 +0200

    ci(hw): proper report creation

--- hardware/target/open-synth-and-pnr/synth-and-pnr.mk
+++ hardware/target/open-synth-and-pnr/synth-and-pnr.mk
@@ -14,8 +14,16 @@ hw-targets = $(addprefix halut-open-synth-and-pnr-, $(TOPLEVELS))
 
 .PHONY: $(hw-targets)
 
+UNIQUE_TOP = $$( \
+			if [ "$*" = "halut_matmul" ]; then \
+				echo "halut_matmul"; \
+			else \
+				sed -n 's|module \($*__[[:alnum:]_]*\)\s.*$$|\1|p' $(HALUT_ROOT)/target/open-frontend/pickle/out/halut_matmul.sv2v.v 2> /dev/null | tail -1; \
+			fi)
+
 $(PNR_DIR)/out/%/.done: $(HALUT_ROOT)/target/open-frontend/pickle/out/halut_matmul.sv2v.v
 	mkdir -p $(dir $@)
+	echo $(UNIQUE_TOP)
 	make -C $(FLOW_HOME) finish \
 		HALUT_ROOT=$(HALUT_ROOT) \
 		DESIGN_CONFIG=$(PNR_DIR)/nangate45/config.mk \
@@ -23,23 +31,18 @@ $(PNR_DIR)/out/%/.done: $(HALUT_ROOT)/target/open-frontend/pickle/out/halut_matm
 		SHELL=/bin/bash \
 		YOSYS_CMD=yosys \
 		OPENROAD_EXE=openroad \
-		DESIGN_NAME=$$( \
-			if [ "$*" = "halut_matmul" ]; then \
-				echo "halut_matmul"; \
-			else \
-				sed -n 's|module \($*__[[:alnum:]_]*\)\s.*$$|\1|p' $(HALUT_ROOT)/target/open-frontend/pickle/out/halut_matmul.sv2v.v 2> /dev/null | tail -1; \
-			fi)
-	python $(FLOW_HOME)/util/genMetrics.py --design $*
+		DESIGN_NAME=$(UNIQUE_TOP)
+	python $(FLOW_HOME)/util/genMetrics.py --design $(UNIQUE_TOP)
 	python $(FLOW_HOME)/util/genReport.py -vvvv
 	python $(FLOW_HOME)/util/genReportTable.py
 	mkdir -p $(dir $@)/results
 	mkdir -p $(dir $@)/reports
-	cp -r $(FLOW_HOME)/reports/nangate45/$*/base/* $(dir $@)/reports
-	cp $(FLOW_HOME)/reports/report-gallery-$*.html $(dir $@)/reports/report-gallery-$*.html
+	cp -r $(FLOW_HOME)/reports/nangate45/$(UNIQUE_TOP)/base/* $(dir $@)/reports
+	cp $(FLOW_HOME)/reports/report-gallery-$(UNIQUE_TOP).html $(dir $@)/reports/report-gallery-$*.html
 	# sed replace the path to the report-gallery.html
-	sed -i 's/nangate45\/$*\/base\///g' $(dir $@)/reports/report-gallery-$*.html
-	cp $(FLOW_HOME)/results/nangate45/$*/base/6_final.v $(dir $@)/results/6_final.v
-	cp $(FLOW_HOME)/results/nangate45/$*/base/6_final.gds $(dir $@)/results/6_final.gds
+	sed -i 's/nangate45\/$*__.+\/base\///g' $(dir $@)/reports/report-gallery-$*.html
+	cp $(FLOW_HOME)/results/nangate45/$(UNIQUE_TOP)/base/6_final.v $(dir $@)/results/6_final.v
+	cp $(FLOW_HOME)/results/nangate45/$(UNIQUE_TOP)/base/6_final.gds $(dir $@)/results/6_final.gds
 	touch $@
 
 $(hw-targets): halut-open-synth-and-pnr-%: $(PNR_DIR)/out/%/.done
