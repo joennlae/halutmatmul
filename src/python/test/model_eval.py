5b98b409fef635a38181951fd65b3b3d0f83af49
Jannis Schönleber <joennlae@gmail.com>
2023-09-14 21:50:36 +0200

    test(eval): add 90%+ model eval test

---
+++ src/python/test/model_eval.py
@@ -0,0 +1,93 @@
+# simple model eval script
+import sys
+import tempfile
+import requests
+
+import torch
+from torch import nn
+from training.train import evaluate
+from retraining import load_model
+
+from halutmatmul.modules import HalutConv2d, HalutLinear
+
+
+def download_model(
+    path: str,
+    is_ci: bool = False,
+    url: str = "https://iis-people.ee.ethz.ch/~janniss/resnet9-best.pth",
+) -> None:
+    # Streaming, so we can iterate over the response.
+    # pylint: disable=missing-timeout
+    r = requests.get(url, stream=True)
+
+    # Total size in Mebibyte
+    total_size = int(r.headers.get("content-length", 0))
+    block_size = 2**20  # Mebibyte
+
+    dl = 0
+    print("Download ResNet-9 CIFAR-10 luts, weights, thresholds, and biases")
+    with open(path + "/" + "resnet9-best.pth", "wb") as f:
+        for data in r.iter_content(block_size):
+            f.write(data)
+            dl += len(data)
+            done = int(50 * dl / total_size)
+            if not is_ci:
+                sys.stdout.write("\r[%s%s]" % ("=" * done, " " * (50 - done)))
+                sys.stdout.flush()
+    print("\n")
+
+
+with tempfile.TemporaryDirectory() as tmpdirname:
+    print("created temporary directory", tmpdirname)
+    download_model(tmpdirname, is_ci=False)
+
+    script_dir = tmpdirname
+    state_dict = torch.load(
+        script_dir + "/" + "resnet9-best" + ".pth", map_location="cpu"
+    )
+
+    (
+        model_name,
+        model,
+        state_dict,
+        data_loader_train,
+        data_loader_val,
+        args_checkpoint,
+        halut_modules,
+        checkpoint,
+    ) = load_model(script_dir + "/" + "resnet9-best" + ".pth")
+    print(model)
+    print(halut_modules)
+
+    model.to("cuda")
+    criterion = nn.CrossEntropyLoss()
+    evaluate(
+        model,
+        criterion=criterion,
+        data_loader=data_loader_val,
+        device="cuda",
+    )
+
+    # int8 quantized model
+    model_int8 = torch.ao.quantization.quantize_dynamic(
+        model,
+        {HalutConv2d, HalutLinear},
+        dtype=torch.qint8,
+    )
+
+    model_int8.to("cuda")
+    criterion = nn.CrossEntropyLoss()
+
+    # run inference on first 100 images
+    # res = model(torch.from_numpy(data_loader_val.dataset.data[:100]).to("cuda"))
+    # res_int8 = model_int8(
+    #     torch.from_numpy(data_loader_val.dataset.data[:100]).to("cuda")
+    # )
+
+    print(model_int8)
+    evaluate(
+        model_int8,
+        criterion=criterion,
+        data_loader=data_loader_val,
+        device="cuda",
+    )
