3c1e296bd6778cb33055c172ece5489780de6a37
Jannis Schönleber <joennlae@gmail.com>
2023-09-18 18:14:50 +0200

    refactor: remove `maddness` folder and initial exploration

--- maddness/download_assets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf assets
-mkdir -p assets
-cd assets
-curl -L -o repo.tar.gz https://github.com/joennlae/bolt/archive/7902ce4c698f7464a515036bf552c6fafb95a5e9.tar.gz
-tar -xvf repo.tar.gz --strip-components=3 bolt-7902ce4c698f7464a515036bf552c6fafb95a5e9/experiments/assets
-rm repo.tar.gz
-rm -rf assets/convnet1m
