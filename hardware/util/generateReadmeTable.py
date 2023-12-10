f4eee660da40c175ae4dc0062364605636975cf1
Jannis Schönleber <joennlae@gmail.com>
2023-12-10 18:20:26 +0100

    docs: update readme table gen script

--- hardware/util/generateReadmeTable.py
+++ hardware/util/generateReadmeTable.py
@@ -54,25 +54,19 @@ with tempfile.TemporaryDirectory() as tmpdirname:
 
     clone_git_repo(github_repo_url, tmpdirname)
 
-    for idx, unit in enumerate(units):
-        # area_asap7 = 0
-        area_nangate45 = 0
-        # tns_asap7 = 0
-        tns_nangate45 = 0
-        # clk_period_asap7 = 0
-        clk_period_nangate45 = 0
-        # std_cells_count_asap7 = 0
-        std_cells_count_nangate45 = 0
-        # utilization_asap7 = 0
-        utilization_nangate45 = 0
+    area_nangate45_list = []
+    freq_nangate45_list = []
+    ge_count_nangate45_list = []
+    std_cells_count_nangate45_list = []
+    util_nangate45_list = []
+    tns_nangate45_list = []
 
-        # with open(f"{tmpdirname}/latest/asap7/{unit}/metrics.json") as f:
-        #     data = json.load(f)
-        #     area_asap7 = data[0]["finish__design__instance__area"]
-        #     tns_asap7 = data[0]["finish__timing__setup__tns"]
-        #     clk_period_asap7 = data[0]["constraints__clocks__details"][0].split(" ")[1]
-        #     std_cells_count_asap7 = data[0]["finish__design__instance__count__stdcell"]
-        #     utilization_asap7 = data[0]["finish__design__instance__utilization"]
+    for idx, unit in enumerate(units):
+        area_nangate45 = 0
+        tns_nangate45 = 0
+        clk_period_nangate45 = 0
+        std_cells_count_nangate45 = 0
+        utilization_nangate45 = 0
 
         with open(f"{tmpdirname}/latest/nangate45/{unit}/reports/metadata.json") as f:
             data = json.load(f)
@@ -82,37 +76,33 @@ with tempfile.TemporaryDirectory() as tmpdirname:
             std_cells_count_nangate45 = data["finish__design__instance__count__stdcell"]
             utilization_nangate45 = data["finish__design__instance__utilization"]
 
-        # freq_asap7 = 1.0 / (float(clk_period_asap7) * (1e-6))
         freq_nangate45 = 1.0 / (float(clk_period_nangate45) * (1e-3))
 
-        # util_asap7 = float(utilization_asap7) * 100
         util_nangate45 = float(utilization_nangate45) * 100
 
-        # GE
-        # ASAP7 NAND2x1_ASAP7_75t_L -> 0.08748
-        # ge_asap7 = 0.08748
-        # NanGate45 NAND2_X1 -> 0.798000
         ge_nangate45 = 0.798000
-
-        # ge_count_asap7 = int(area_asap7 / ge_asap7)
         ge_count_nangate45 = int(area_nangate45 / ge_nangate45)
 
-        current_table = f"""
-### {titles[idx]}
-| {unit}         |  NanGate45      |
+        area_nangate45_list.append(area_nangate45)
+        freq_nangate45_list.append(freq_nangate45)
+        ge_count_nangate45_list.append(ge_count_nangate45)
+        std_cells_count_nangate45_list.append(std_cells_count_nangate45)
+        util_nangate45_list.append(util_nangate45)
+        tns_nangate45_list.append(tns_nangate45)
+
+    table_string = f"""
+| NanGate45      |  {units[0]}  |  {units[1]}  |  {units[2]}  |
 | -------------  |  -------------  |
-| Area [μm^2]    | {area_nangate45} |
-| Freq [Mhz]     |  {freq_nangate45:.1f} |
-| GE             |  {format_ge(ge_count_nangate45)} |
-| Std Cell [#]   |  {std_cells_count_nangate45} | 
+| Area [μm^2]    |  {area_nangate45_list[0]} |  {area_nangate45_list[1]}  |  {area_nangate45_list[2]}  | 
+| Freq [Mhz]     |  {freq_nangate45_list[0]:.1f} |  {freq_nangate45_list[1]:.1f}  |  {freq_nangate45_list[2]:.1f}  |
+| GE             |  {format_ge(ge_count_nangate45_list[0])} | {format_ge(ge_count_nangate45_list[1])}  |  {format_ge(ge_count_nangate45_list[2])}  |
+| Std Cell [#]   |  {std_cells_count_nangate45_list[0]} |  {std_cells_count_nangate45_list[1]}  |  {std_cells_count_nangate45_list[2]}  |
 | Voltage [V]    |   1.1             |
-| Util [%]       |  {util_nangate45:.1f} | 
-| TNS            |  {tns_nangate45} |
-| Clock Net      | ![Clock_net]({base_raw_url}main/latest/nangate45/{unit}/reports/final_clocks.webp)  |
-| Routing        | ![Routing]({base_raw_url}main/latest/nangate45/{unit}/reports/final_routing.webp)  |
-| GDS            | [GDS Download]({base_raw_url}main/latest/nangate45/{unit}/results/6_final.gds)  |
-
+| Util [%]       |  {util_nangate45_list[0]:.1f} |  {util_nangate45_list[1]:.1f}  |  {util_nangate45_list[2]:.1f}  |
+| TNS            |  {tns_nangate45_list[0]} |  {tns_nangate45_list[1]}  |  {tns_nangate45_list[2]}  |
+| Clock Net      | <img src="{base_raw_url}main/latest/nangate45/{units[0]}/reports/final_clocks.webp" alt="Clock Net" width="150">  | <img src="{base_raw_url}main/latest/nangate45/{units[1]}/reports/final_clocks.webp" alt="Clock Net" width="150">  | <img src="{base_raw_url}main/latest/nangate45/{units[2]}/reports/final_clocks.webp" alt="Clock Net" width="150">  |
+| Routing        | <img src="{base_raw_url}main/latest/nangate45/{units[0]}/reports/final_routing.webp" alt="Routing" width="150">   | <img src="{base_raw_url}main/latest/nangate45/{units[1]}/reports/final_routing.webp" alt="Routing" width="150">   | <img src="{base_raw_url}main/latest/nangate45/{units[2]}/reports/final_routing.webp" alt="Routing" width="150">   |
+| GDS            | [GDS Download]({base_raw_url}main/latest/nangate45/{units[0]}/results/6_final.gds)  | [GDS Download]({base_raw_url}main/latest/nangate45/{units[1]}/results/6_final.gds)  | [GDS Download]({base_raw_url}main/latest/nangate45/{units[2]}/results/6_final.gds)  |
 """
-        resulting_string += current_table
 
-    print(resulting_string)
+    print(table_string)
